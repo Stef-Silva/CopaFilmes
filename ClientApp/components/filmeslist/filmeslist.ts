2b81f9754a971e0ecaab865437e962cf9bf907ee
Stef-Silva <st3f.silva@gmail.com>
2019-04-13 08:03:46 -0300

    Lógica do front implemented

--- ClientApp/components/filmeslist/filmeslist.ts
+++ ClientApp/components/filmeslist/filmeslist.ts
@@ -12,6 +12,7 @@ interface Filmes {
 @Component
 export default class FilmesListComponent extends Vue {
     forecasts: Filmes[] = [];   
+    filmes = [];
     created(){
         axios.get('https://copadosfilmes.azurewebsites.net/api/filmes')
         .then(response => {
@@ -20,7 +21,20 @@ export default class FilmesListComponent extends Vue {
         .catch(error => {
             console.log(error)
         })
-    } 
+    }    
+    sendFilmes(){
+        if(this.filmes.length == 0){
+            alert('Não há filmes selecionados!!!')
+        }else if(this.filmes.length > 8){
+            alert('O número de filmes selecionados ultrapassou!!!')
+        }else if(this.filmes.length > 0 && this.filmes.length < 8){
+            alert('Selecionar 8 filmes!')
+        }else{
+            console.log('POST')
+            console.log(this.filmes)
+            console.log(this.filmes.length)
+        }        
+    }
     mounted() {
         /*fetch('')
             .then(response => response.json() as Promise<Filmes[]>)
@@ -29,8 +43,8 @@ export default class FilmesListComponent extends Vue {
             });*/
     }  
     data(){
-        return{
-            filmes: []
+        return{            
+            filmes: []            
         }
     }  
 }
